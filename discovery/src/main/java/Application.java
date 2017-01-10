7095961fceb2271170f209fd859525b0012a35f5
Craig Tubb <craig.tubb@mydrivesolutions.com>
2017-01-10 23:16:09 +0000

    Include Eureka service in discovery application

--- discovery/src/main/java/Application.java
+++ discovery/src/main/java/Application.java
@@ -2,8 +2,10 @@ package services;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
