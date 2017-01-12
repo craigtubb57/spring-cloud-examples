acd6651f44d1f50408805e64203ddd4d972c975b
Craig Tubb <craig.tubb@mydrivesolutions.com>
2017-01-12 15:54:34 +0000

    Define UserRepository for database, with available actions

---
+++ users/src/main/java/UserRepository.java
@@ -0,0 +1,17 @@
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.List;
+
+@RepositoryRestResource(collectionResourceRel = "users", path = "users")
+public interface UserRepository extends PagingAndSortingRepository<User, Long> {
+
+    void addUser(User user);
+
+    void updateUser(User user);
+
+    void removeUser(User user);
+
+    List<User> findUsers(@Param("firstName") String firstName, @Param("lastName") String lastName, @Param("emailAddress") String emailAddress);
+}
