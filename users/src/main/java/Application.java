73fa77dae3e700b043bf2c86bbba24e710e6e046
Craig Tubb <craig.tubb@mydrivesolutions.com>
2017-01-12 15:42:46 +0000

    Basic Application file for Users Service

---
+++ users/src/main/java/Application.java
@@ -0,0 +1,27 @@
+package data;
+
+import data.domain.nodes.User;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableZuulProxy
+public class Application {
+
+    public Application() {
+        setBasePackage("java");
+    }
+
+    public static void main(String[] args) {
+        ConfigurableApplicationContext ctx = SpringApplication.run(Application.class, args);
+
+        RepositoryRestConfiguration restConfiguration = ctx.getBean("config", RepositoryRestConfiguration.class);
+        restConfiguration.exposeIdsFor(User.class);
+    }
+}
